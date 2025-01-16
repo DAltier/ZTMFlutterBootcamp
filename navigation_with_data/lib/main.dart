4f01e71ff4e89628c02d773fed925d6d2998fba3
DAltier <altierdana@gmail.com>
2025-01-15 21:09:59 -0800

    navigation with data

---
+++ navigation_with_data/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:context/pages/first.dart';
+import 'package:context/pages/second.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const MyHomePage(),
+        '/first': (context) => const FirstPage(),
+        '/second': (context) => const SecondPage(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Navigation"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const FirstPage(name: "Tadas"),
+                  ),
+                );
+              },
+              child: const Text("First Page"),
+            ),
+            TextButton(
+              onPressed: () async {
+                final response = await Navigator.pushNamed(context, "/second",
+                    arguments: "Andrei");
+                print(response);
+              },
+              child: const Text("Second Page"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
